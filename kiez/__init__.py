cc973c7bdba9475ea2fa63b3d967b3d72dfef6d7
Daniel Obraczka <obraczka@informatik.uni-leipzig.de>
2021-06-17 13:21:43 +0200

    Minor comment for test coverage

--- kiez/__init__.py
+++ kiez/__init__.py
@@ -1,9 +1,9 @@
 import sys
 
 if sys.version_info[1] > 7:
-    from importlib.metadata import version
+    from importlib.metadata import version  # pragma: no cover
 else:
-    from importlib_metadata import version
+    from importlib_metadata import version  # pragma: no cover
 
 from kiez.kiez import Kiez
 

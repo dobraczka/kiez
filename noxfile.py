2d9e6a6e8b559f26e6df65c96313f66c00689c1a
Daniel Obraczka <obraczka@informatik.uni-leipzig.de>
2022-05-31 16:56:32 +0200

    Add doc building to GAH

--- noxfile.py
+++ noxfile.py
@@ -16,7 +16,14 @@ locations = ["kiez", "tests", "noxfile.py"]
 @session()
 def lint(session: Session) -> None:
     args = session.posargs or locations
-    session.install("pyproject-flake8", "flake8-eradicate", "flake8-isort", "flake8-debugger", "flake8-comprehensions", "flake8-print")
+    session.install(
+        "pyproject-flake8",
+        "flake8-eradicate",
+        "flake8-isort",
+        "flake8-debugger",
+        "flake8-comprehensions",
+        "flake8-print",
+    )
     session.run("pflake8", *args)
 
 
@@ -25,3 +32,13 @@ def type_checking(session: Session) -> None:
     args = session.posargs or locations
     session.install("mypy")
     session.run("mypy", "--ignore-missing-imports", *args)
+
+
+@session()
+def build_docs(session: Session) -> None:
+    session.install(".")
+    session.install("sphinx")
+    session.install("insegel")
+    session.cd("docs")
+    session.run("make", "clean", external=True)
+    session.run("make", "html", external=True)

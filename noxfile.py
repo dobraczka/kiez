4e5b6d84c531545909fc3365677944e871b1cf71
Daniel Obraczka <daniel@obraczka.de>
2023-03-30 14:07:22 +0200

    Improve CI, test install on all python versions and oses

--- noxfile.py
+++ noxfile.py
@@ -1,7 +1,7 @@
 from nox_poetry import Session, session
 
 
-@session()
+@session(tags=["test"])
 def tests(session: Session) -> None:
     args = session.posargs or ["--cov=kiez/", "--cov-report=xml", "tests/"]
     session.install(".[all]")
@@ -13,8 +13,16 @@ def tests(session: Session) -> None:
 locations = ["kiez", "tests", "noxfile.py"]
 
 
-@session()
+@session(tags=["fix"])
 def lint(session: Session) -> None:
+    args = session.posargs or locations
+    session.install("black", "isort")
+    session.run("black", *args)
+    session.run("isort", *args)
+
+
+@session(tags=["style"])
+def style_checking(session: Session) -> None:
     args = session.posargs or locations
     session.install(
         "pyproject-flake8",
@@ -27,13 +35,13 @@ def lint(session: Session) -> None:
     session.run("pflake8", *args)
 
 
-@session()
+@session(tags=["style"])
 def pyroma(session: Session) -> None:
     session.install("poetry", "pyroma")
     session.run("pyroma", "--min", "10", ".")
 
 
-@session()
+@session(tags=["style", "docs"])
 def doctests(session: Session) -> None:
     session.install(".[all]")
     session.install("xdoctest")
@@ -41,14 +49,14 @@ def doctests(session: Session) -> None:
     session.run("xdoctest", "-m", "kiez")
 
 
-@session()
+@session(tags=["style"])
 def type_checking(session: Session) -> None:
     args = session.posargs or locations
     session.install("mypy")
     session.run("mypy", "--ignore-missing-imports", *args)
 
 
-@session()
+@session(tags=["docs"])
 def build_docs(session: Session) -> None:
     session.install(".")
     session.install("sphinx")
